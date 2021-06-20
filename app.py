a749f41669f3ae8b8d1047035532ed0398ceb9cb
SymonRod <simone.rodino@gmail.com>
2021-06-21 00:43:25 +0200

    Added login logout

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, redirect, url_for
-from flask import Flask, render_template
-from flask import request
-from flask import jsonify
-from db import session, Nomi, MiPiace
+import random
+import string
+from app_secret import secret
+from flask import Flask, render_template, request, jsonify, url_for, redirect
+from flask import session as flask_session
+from sqlalchemy.sql.functions import user
+from db import session, Nomi, MiPiace, UserTest
+from passlib.hash import pbkdf2_sha256
 
 
 app = Flask(__name__, static_url_path='',)
+
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
 
 @app.route("/newUtente")
@@ -28,7 +35,79 @@ def getLikes():
     return jsonify(likes)
 
 
-@app.route("/api/getNames")
+@app.route("/login")
+def verifyUser():
+    try:
+        username = request.args["username"]
+        passwd = request.args["password"]
+    except Exception as e:
+        return "Error"
+
+    for user in session.query(UserTest.nickname, UserTest.password, UserTest.salt).filter(UserTest.nickname == username):
+        passwd = pbkdf2_sha256.hash(
+            passwd, rounds=200000, salt=user.salt.encode())
+
+        if passwd == user.password:
+            flask_session['logged'] = True
+            flask_session['nickname'] = user.nickname
+            return redirect("/")
+
+    return "Username o password incorretti!"
+
+
+@app.route("/addUser")
+def addUser():
+    try:
+        username = request.args["username"]
+        passwd = request.args["password"]
+        if("salt" in request.args.keys()):
+            salt = request.args["salt"]
+        else:
+            salt = id_generator(40)
+    except Exception as e:
+        return "Error"
+
+    for user in session.query(UserTest.nickname).filter(UserTest.nickname == username):
+        return 'Username alredy exits!'
+
+    passwd = pbkdf2_sha256.hash(passwd, rounds=200000, salt=salt.encode())
+
+    user = UserTest(nickname=username, password=passwd, salt=salt)
+
+    session.add(user)
+
+    session.commit()
+
+    return "User added!"
+
+
+# @app.route("/testhash")
+# def testhash():
+#     try:
+#         passwd = request.args["passwd"]
+#         if("salt" in request.args.keys()):
+#             salt = request.args["salt"]
+#         else:
+#             salt = id_generator(40)
+#     except Exception as e:
+#         return "Error"
+#     passwd = pbkdf2_sha256.hash(passwd, rounds=200000, salt=salt.encode())
+#     return "salt: "+salt+" | pass: "+passwd
+
+
+@app.route("/logout")
+def logout():
+    vars = []
+    for key in flask_session:
+        vars.append(key)
+
+    for key in vars:
+        flask_session.pop(key)
+
+    return redirect("/")
+
+
+@ app.route("/api/getNames")
 def getNames():
     try:
         search_name = request.args["name"]
@@ -51,10 +130,11 @@ def getNames():
     return jsonify(nomi)
 
 
-@app.route("/")
+@ app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", session=flask_session)
 
 
 if __name__ == "__main__":
+    app.secret_key = secret
     app.run(debug=True)
--- db.py
+++ db.py
@@ -15,14 +15,14 @@ session = Session()
 
 class Nomi(Base):
     __tablename__ = 'Nomi'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     nome = Column(String)
     id_lingua = Column(Integer)
 
 
 class MiPiace(Base):
     __tablename__ = 'MiPiace'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     id_utente = Column(Integer)
     id_nome = Column(Integer)
     date = Column(DateTime)
@@ -31,14 +31,26 @@ class MiPiace(Base):
 
 class Lingue(Base):
     __tablename__ = 'Lingue'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     lingua = Column(String)
     valore = Column(String)
 
 
 class Dettagli(Base):
     __tablename__ = 'Dettagli'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String)
     fullname = Column(String)
     nickname = Column(String)
+
+
+class UserTest(Base):
+    __tablename__ = 'UserTest'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nickname = Column(String)
+    password = Column(String)
+    salt = Column(String)
+
+
+if __name__ == "__main__":
+    Base.metadata.create_all(engine)
