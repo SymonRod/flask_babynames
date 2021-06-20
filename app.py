4b57f90f2e3a3d43dee229610d3b209d559deada
SymonRod <simone.rodino@gmail.com>
2021-06-20 21:04:57 +0200

    Added getNames

--- app.py
+++ app.py
@@ -1,56 +1,55 @@
 from flask import Flask, redirect, url_for
 from flask import Flask, render_template
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
 from flask import request
-
-Base = declarative_base()
-
-
-class User(Base):
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    fullname = Column(String)
-    nickname = Column(String)
-
-    def __repr__(self):
-        return "<User(name='%s', fullname='%s', nickname='%s')>" % (self.name, self.fullname, self.nickname)
+from flask import jsonify
+from db import session, Nomi, MiPiace
 
 
 app = Flask(__name__, static_url_path='',)
-
-engine = create_engine('sqlite:///database/db.sqlite',
-                       echo=True, connect_args={'check_same_thread': False})
-
-Session = sessionmaker()
-Session.configure(bind=engine)
-session = Session()
 
 
 @app.route("/newUtente")
 def newUtente():
     return render_template("index.html", content="Testing")
 
-# @app.route("/")
-# def home():
 
-# 	users = []
+@app.route("/api/getLikes")
+def getLikes():
+    try:
+        id_nome = request.args["idNome"]
+    except Exception as e:
+        return "Error"
 
-# 	for instance in session.query(User).order_by(User.id):
-# 		users.append(instance)
-# 		print(instance.name)
+    likes = {'likes': []}
+
+    for instance in session.query().filter(MiPiace.valore, MiPiace.id_nome == id_nome).order_by(Nomi.id):
+        likes['likes'].append({"value": instance.valore})
+
+    jsonify(likes)
 
 
-# 	data = {'users':users}
-# 	return render_template("index.html", content="Testing", data=data)
+@app.route("/api/getNames")
+def getNames():
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
+    try:
+        search_name = request.args["name"]
+        if search_name == "":
+            raise Exception("Empty search are not allowed")
+        languages = request.args["language"]
+        languages = languages.split(",")
+
+    except Exception as e:
+        return "Error"
+    nomi = {"nomi": []}
+
+    for instance in session.query(Nomi.nome, Nomi.id_lingua, Nomi.id).filter(Nomi.nome.like(search_name+"%")).order_by(Nomi.id):
+        nome = {"nome": {
+            "value": instance.nome,
+            "language": instance.id_lingua,
+            "id": instance.id}}
+        nomi["nomi"].append(nome)
+
+    return jsonify(nomi)
 
 
 @app.route("/")
---
+++ db.py
@@ -0,0 +1,48 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine
+
+from sqlalchemy.orm import sessionmaker
+
+
+Base = declarative_base()
+engine = create_engine('sqlite:///database/data.sqlite',
+                       echo=True, connect_args={'check_same_thread': False})
+
+Session = sessionmaker()
+Session.configure(bind=engine)
+session = Session()
+
+
+class Nomi(Base):
+    __tablename__ = 'Nomi'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String)
+    id_lingua = Column(Integer)
+
+
+class MiPiace(Base):
+    __tablename__ = 'MiPiace'
+
+    id = Column(Integer, primary_key=True)
+    id_utente = Column(Integer)
+    id_nome = Column(Integer)
+    date = Column(DateTime)
+    valore = Column(Integer)
+
+
+class Lingue(Base):
+    __tablename__ = 'Lingue'
+
+    id = Column(Integer, primary_key=True)
+    lingua = Column(String)
+    valore = Column(String)
+
+
+class Dettagli(Base):
+    __tablename__ = 'Dettagli'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    nickname = Column(String)
